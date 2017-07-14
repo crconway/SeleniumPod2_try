7526d334ca429f2b502d66b8ea3980b215e4dc87
Chris Conway <crconway@me.com>
2017-07-14 14:26:21 -0400

    added StoreOverlay and PIP

---
+++ src/test/java/StoreOverlay.java
@@ -0,0 +1,20 @@
+/**
+ * Created by crc07 on 7/13/17.
+ */
+public class StoreOverlay extends ProductInfoPage {
+
+    public static final String PRODUCT_TITLE_STORE = ".//h2[@class='u__medium' and contains(text(), 'TO_REPLACE')]";
+    public static final String ADD_TO_CART_STORE = ".//span[@class='bttn__content' and text() = 'Add To Cart']";
+    public static final String QTY_BOX = "(.//input[@type='tel'])[1]";
+    public static final String STORE_QTY = "(.//h3[@class='u__medium u__text--success store__name--inline'])[1]";
+
+
+    //Create Wrapper for store list
+
+    //For loop to verify qty greater than 0
+
+    //add qty 1 to qty box
+
+    //add to cart
+
+}
